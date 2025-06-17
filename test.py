a78277e374aded16429aa6b341f0dbbcb7c79fca
sai23082004 <ganjikuntasaikiran2977@gmail.com>
2025-06-17 15:44:15 +0530

    version

---
+++ test.py
@@ -0,0 +1,5 @@
+def main():
+    n=input("enter the no of students ")
+    
+if __name__ == '__main__':
+    main()
---
+++ test2.py
@@ -0,0 +1,8 @@
+command =""
+while True:
+    command = input(">").lower
+    if command=="help":
+        print('''
+start- to start the car
+stop- to stop the car 
+quit - to quit ''')
